b5fb9a7465c967f43cbb30db01d027aa968a9746
atem91600 <atem91600@gmail.com>
2016-05-16 19:22:31 +0200

    Update doublon.py

--- doublon.py
+++ doublon.py
@@ -4,7 +4,7 @@
 # Projet Sudoku : Module error
 #--------------------------------------
 
-def doublon(l):
+def doublon(l:list):
     """
     Fonction qui va chercher les répétition dans une liste représentant une 
     grille de Sudoku.
@@ -19,18 +19,50 @@ def doublon(l):
 	resligne = []
 	rescolonne = []
 	for x in range(9):
+		resinter = 0
 		for i in range (1,10):
 			if l[x].count(i) > 1:
-				repetition = l[x].count(i)
-				resinter = ['l',x,i,repetition]
-				resligne.append(resinter)
+				resinter += 1
+		resligne.append(resinter)
 	for y in range(9):
 		listinter = []
+		resinter = 0
 		for x in range(9):
 			listinter.append(l[x][y])
 		for i in range(1,10):
 			if listinter.count(i) > 1:
-				repetition = listinter.count(i)
-				resinter = ['c',y,i,repetition]
-				rescolonne.append(resinter)
+				resinter += 1
+		rescolonne.append(resinter)
 	return (resligne,rescolonne)
+
+
+def doub_bloc(l:list):
+	repet = doublon(l)
+	blocs = [0]*9
+	for i in range (3):
+		blocs[0] += repet[0][i] + repet[1][i]
+		blocs[1] += repet[0][i]
+		blocs[2] += repet[0][i]
+		blocs[3] += repet[1][i]
+		blocs[6] += repet[1][i]
+	for i in range(3,6):
+		blocs[1] += repet[1][i]
+		blocs[3] += repet[0][i]
+		blocs[4] += repet[0][i] + repet[1][i]
+		blocs[5] += repet[0][i]
+		blocs[7] += repet[1][i]
+	for i in range(6,9):
+		blocs[2] += repet[1][i]
+		blocs[5] += repet[1][i]
+		blocs[6] += repet[0][i]
+		blocs[7] += repet[0][i]
+		blocs[8] += repet[0][i] + repet[1][i]
+
+	inter = 0
+	for j in range(1,8):
+		if blocs[j]>blocs[inter]:
+			inter = j
+			
+	return inter
+
+
