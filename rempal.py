e5f4c20f50512576081d6a9fe68c601d9892a1bc
Alexandre <alexandre.alex10@hotmail.fr>
2016-05-16 18:26:02 +0200

    Module Remplissage

--- rempal.py
+++ rempal.py
@@ -39,20 +39,23 @@ def nb_dispo(liste):
 	return fin
 
 def liste_finale(liste):
-	liste_fin = []
+	fin = []
 	for i in range(3):
+		ia = 0
 		if i == 1:
-			x = 3
+			ia = 3
 		if i == 2:
-			x = 6
-		liste1 = liste[x]
-		liste2 = liste[x+1]
-		liste3 = liste[x+2]
-		for j in range(3):
-			for k in range(3):
-				liste_fin.append(liste1)
-				liste_fin.append(liste1)
-				liste_fin.append(liste1)
+			ia = 6
+		liste1 = liste[ia]
+		liste2 = liste[ia+1]
+		liste3 = liste[ia+2]
+		ligne1 = [liste1[0], liste1[1], liste1[2], liste2[0], liste2[1], liste2[2], liste3[0], liste3[1], liste3[2]]
+		ligne2 = [liste1[3], liste1[4], liste1[5], liste2[3], liste2[4], liste2[5], liste3[3], liste3[4], liste3[5]]
+		ligne3 = [liste1[6], liste1[7], liste1[8], liste2[6], liste2[7], liste2[8], liste3[6], liste3[7], liste3[8]]
+		fin.append(ligne1)
+		fin.append(ligne2)
+		fin.append(ligne3)
+	return fin
 
 def remplissage_total(liste):
 	grande_liste = []
@@ -68,7 +71,7 @@ def remplissage_total(liste):
 				else:
 					liste9.append(liste_bloc[z])
 			grande_liste.append(liste9)
-	grande_liste
-	return liste
+	grande_liste = liste_finale(grande_liste)
+	return grande_liste
  
-print(remplissage_total([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+#print(remplissage_total([[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]))
